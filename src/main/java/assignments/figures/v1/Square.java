b098a36f3f428ba94ecbb232ae77962bac50e8fa
zhirea <zhirea2022@gmail.com>
2025-10-28 14:21:49 +0400

    gitlab.ci and pom update, adding figures v1

---
+++ src/main/java/assignments/figures/v1/Square.java
@@ -0,0 +1,106 @@
+package assignments.figures.v1;
+
+class Square {
+    private Point topLeft;
+    private int size;
+
+    public Square(Point leftTop, int size) {
+        if (size <= 0) throw new IllegalArgumentException("Size must be positive");
+        this.topLeft = leftTop;
+        this.size = size;
+    }
+
+    public Square(int xLeft, int yTop, int size) {
+        if (size <= 0) throw new IllegalArgumentException("Size must be positive");
+        this.topLeft = new Point(xLeft, yTop);
+        this.size = size;
+    }
+
+    public Square(int size) {
+        if (size <= 0) throw new IllegalArgumentException("Size must be positive");
+        this.topLeft = new Point(0, -10);
+        this.size = size;
+    }
+
+    public Square() {
+        this.topLeft = new Point(0, -1);
+        this.size = 1;
+    }
+
+    public Point getTopLeft() {
+        return topLeft;
+    }
+
+    public Point getBottomRight() {
+        return new Point(topLeft.getX() + size, topLeft.getY() + size);
+    }
+
+    public void setTopLeft(Point topLeft) {
+        this.topLeft = topLeft;
+    }
+
+    public int getLength() {
+        return size;
+    }
+
+    public void moveTo(int x, int y) {
+        this.topLeft = new Point(x, y);
+    }
+
+    public void moveTo(Point point) {
+        moveTo(point.getX(), point.getY());
+    }
+
+    public void moveRel(int dx, int dy) {
+        moveTo(topLeft.getX() + dx, topLeft.getY() + dy);
+    }
+
+    public void resize(double ratio) {
+        if (ratio <= 0) throw new IllegalArgumentException("Ratio must be positive");
+        size = (int) (size * ratio);
+    }
+
+    public double getArea() {
+        return size * size;
+    }
+
+    public double getPerimeter() {
+        return 4 * size;
+    }
+
+    public boolean isInside(int x, int y) {
+        return (x >= topLeft.getX() && x <= topLeft.getX() + size &&
+                y >= topLeft.getY() && y <= topLeft.getY() + size);
+    }
+
+    public boolean isInside(Point point) {
+        return isInside(point.getX(), point.getY());
+    }
+
+    public boolean isIntersects(Square square) {
+        return !(getBottomRight().getX() < square.topLeft.getX() ||
+                topLeft.getX() > square.getBottomRight().getX() ||
+                getBottomRight().getY() < square.topLeft.getY() ||
+                topLeft.getY() > square.getBottomRight().getY());
+    }
+
+    public boolean isInside(Square square) {
+        return isInside(square.topLeft) &&
+                isInside(square.getBottomRight());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Square square = (Square) obj;
+        return size == square.size && topLeft.equals(square.topLeft);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = topLeft.hashCode();
+        result = 31 * result + size;
+        return result;
+    }
+}
